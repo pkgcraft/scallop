fa5232515da87be4b5a5ea84c2285d5c6de498d1
Tim Harder <radhermit@gmail.com>
2022-01-02 14:11:18 -0700

    rust: use absolute paths for scallop lib/header link args

--- rust/build.rs
+++ rust/build.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::path::PathBuf;
 
 use bindgen::callbacks::ParseCallbacks;
@@ -21,13 +22,21 @@ impl ParseCallbacks for BashCallback {
 
 fn main() {
     // generate scallop-specific bindings
-    // link to the scallop library
-    println!("cargo:rustc-link-lib=scallop");
-    println!("cargo:rerun-if-changed=../src/scallop.h");
+    let repo_dir_path = fs::canonicalize(format!("{}/../", env!("CARGO_MANIFEST_DIR"))).unwrap();
+    let repo_dir = repo_dir_path.to_str().unwrap();
+    let scallop_build_dir = format!("{}/build/src", repo_dir);
+    let scallop_src_dir = format!("{}/src", repo_dir);
+    let scallop_header = format!("{}/scallop.h", scallop_src_dir);
+    println!("cargo:rustc-link-search=native={}", scallop_build_dir);
+    println!("cargo:rustc-link-lib=dylib=scallop");
+    println!("cargo:rerun-if-changed={}", scallop_header);
+
+    // https://github.com/rust-lang/cargo/issues/4895
+    println!("cargo:rustc-env=LD_LIBRARY_PATH={}", scallop_build_dir);
 
     let bindings = bindgen::Builder::default()
         // header to generate bindings for
-        .header("../src/scallop.h")
+        .header(scallop_header)
         // invalidate built crate whenever any included header file changes
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
