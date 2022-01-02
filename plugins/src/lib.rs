cec76c7b537c0160df4ec45bde4a0ff7229cb94d
Tim Harder <radhermit@gmail.com>
2022-01-01 23:47:12 -0700

    plugins: drop unnecessary pub usage for exported C-compatible symbols

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -1,17 +1,18 @@
 use scallop::bash::builtins::Builtin;
 
 #[export_name = "has_struct"]
-pub static mut HAS_STRUCT: Option<Builtin> = None;
+static mut HAS_STRUCT: Option<Builtin> = None;
 #[export_name = "hasv_struct"]
-pub static mut HASV_STRUCT: Option<Builtin> = None;
+static mut HASV_STRUCT: Option<Builtin> = None;
 #[export_name = "ver_rs_struct"]
-pub static mut VER_RS_STRUCT: Option<Builtin> = None;
+static mut VER_RS_STRUCT: Option<Builtin> = None;
 
+#[used]
 #[link_section = ".init_array"]
-pub static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
+static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
 
 #[no_mangle]
-pub extern "C" fn initialize_builtins() {
+extern "C" fn initialize_builtins() {
     unsafe {
         HAS_STRUCT = Some(Builtin::register("has"));
         HASV_STRUCT = Some(Builtin::register("hasv"));
