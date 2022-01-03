c8efa23b929cb3c89828ef23c6e9c737af875e08
Tim Harder <radhermit@gmail.com>
2022-01-03 02:52:31 -0700

    builtins: fix struct constructors on macos

--- builtins/src/base.rs
+++ builtins/src/base.rs
@@ -4,8 +4,14 @@ use scallop::bash::builtins::*;
 #[export_name = "profile_struct"]
 static mut PROFILE_STRUCT: Option<bindings::Builtin> = None;
 
+#[cfg(target_os = "linux")]
 #[used]
 #[link_section = ".init_array"]
+static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
+
+#[cfg(target_os = "macos")]
+#[used]
+#[link_section = "__DATA,__mod_init_func"]
 static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
 
 #[no_mangle]
--- builtins/src/pkgcraft.rs
+++ builtins/src/pkgcraft.rs
@@ -12,8 +12,14 @@ static mut VER_RS_STRUCT: Option<bindings::Builtin> = None;
 #[export_name = "ver_test_struct"]
 static mut VER_TEST_STRUCT: Option<bindings::Builtin> = None;
 
+#[cfg(target_os = "linux")]
 #[used]
 #[link_section = ".init_array"]
+static INITIALIZE_PKGCRAFT_BUILTINS: extern "C" fn() = initialize_pkgcraft_builtins;
+
+#[cfg(target_os = "macos")]
+#[used]
+#[link_section = "__DATA,__mod_init_func"]
 static INITIALIZE_PKGCRAFT_BUILTINS: extern "C" fn() = initialize_pkgcraft_builtins;
 
 #[no_mangle]
