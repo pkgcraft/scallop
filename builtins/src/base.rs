b20b1ee0885bfdc5f6c4376e722a47fe26abf691
Tim Harder <radhermit@gmail.com>
2022-01-02 18:46:33 -0700

    builtins: split base builtins into a separate file

---
+++ builtins/src/base.rs
@@ -0,0 +1,16 @@
+use scallop::bash::bindings;
+use scallop::bash::builtins::*;
+
+#[export_name = "profile_struct"]
+static mut PROFILE_STRUCT: Option<bindings::Builtin> = None;
+
+#[used]
+#[link_section = ".init_array"]
+static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
+
+#[no_mangle]
+extern "C" fn initialize_builtins() {
+    unsafe {
+        PROFILE_STRUCT = Some(profile::BUILTIN.into());
+    }
+}
--- builtins/src/lib.rs
+++ builtins/src/lib.rs
@@ -1,19 +1,3 @@
-use scallop::bash::bindings;
-use scallop::bash::builtins::*;
-
+mod base;
 #[cfg(feature = "pkgcraft")]
-pub mod pkgcraft;
-
-#[export_name = "profile_struct"]
-static mut PROFILE_STRUCT: Option<bindings::Builtin> = None;
-
-#[used]
-#[link_section = ".init_array"]
-static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
-
-#[no_mangle]
-extern "C" fn initialize_builtins() {
-    unsafe {
-        PROFILE_STRUCT = Some(profile::BUILTIN.into());
-    }
-}
+mod pkgcraft;
