25cfa6c6895ab4363b1bd65c7e1333d84720021c
Tim Harder <radhermit@gmail.com>
2022-01-07 14:36:42 -0700

    scallop-builtins: drop pkgcraft-builtins entirely

--- scallop-builtins/src/lib.rs
+++ scallop-builtins/src/lib.rs
@@ -1,3 +1,22 @@
-mod base;
-#[cfg(feature = "pkg")]
-mod pkg;
+use scallop::bash;
+use scallop::builtins::*;
+
+#[export_name = "profile_struct"]
+static mut PROFILE_STRUCT: Option<bash::Builtin> = None;
+
+#[cfg(target_os = "linux")]
+#[used]
+#[link_section = ".init_array"]
+static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
+
+#[cfg(target_os = "macos")]
+#[used]
+#[link_section = "__DATA,__mod_init_func"]
+static INITIALIZE_BUILTINS: extern "C" fn() = initialize_builtins;
+
+#[no_mangle]
+extern "C" fn initialize_builtins() {
+    unsafe {
+        PROFILE_STRUCT = Some(profile::BUILTIN.into());
+    }
+}
