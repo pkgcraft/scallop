20f92edc0e131e9e5328fa56475e2482d1d18cc3
Tim Harder <radhermit@gmail.com>
2022-01-07 14:46:22 -0700

    move interative shell init into the scallop library

--- scallop/src/bash.rs
+++ scallop/src/bash.rs
@@ -1,4 +1,6 @@
+use std::ffi::CString;
 use std::os::raw::{c_char, c_int};
+use std::{env, mem, ptr};
 
 mod internal {
     #![allow(non_camel_case_types)]
@@ -10,7 +12,6 @@ mod internal {
     include!(concat!(env!("OUT_DIR"), "/bash-bindings.rs"));
 }
 
-pub use internal::bash_main;
 pub(crate) use internal::*;
 
 type BuiltinFnPtr = unsafe extern "C" fn(list: *mut WordList) -> c_int;
@@ -29,4 +30,27 @@ pub struct Builtin {
     pub long_doc: *const *mut c_char,
     pub short_doc: *const c_char,
     pub handle: *mut c_char,
+}
+
+/// Start an interactive shell session.
+pub fn shell() -> i32 {
+    let argv_strs: Vec<CString> = env::args().map(|s| CString::new(s).unwrap()).collect();
+    let mut argv_ptrs: Vec<*mut c_char> = argv_strs.iter().map(|s| s.as_ptr() as *mut _).collect();
+    argv_ptrs.push(ptr::null_mut());
+    let argv = argv_ptrs.as_ptr() as *mut _;
+    let argc: c_int = argv_strs.len().try_into().unwrap();
+    mem::forget(argv_strs);
+    mem::forget(argv_ptrs);
+
+    let env_strs: Vec<CString> = env::vars()
+        .map(|(key, val)| format!("{}={}", key, val))
+        .map(|s| CString::new(s).unwrap())
+        .collect();
+    let mut env_ptrs: Vec<*mut c_char> = env_strs.iter().map(|s| s.as_ptr() as *mut _).collect();
+    env_ptrs.push(ptr::null_mut());
+    let env = env_ptrs.as_ptr() as *mut _;
+    mem::forget(env_strs);
+    mem::forget(env_ptrs);
+
+    unsafe { bash_main(argc, argv, env) }
 }
--- scallop/src/main.rs
+++ scallop/src/main.rs
@@ -1,28 +1,7 @@
-use std::ffi::CString;
-use std::os::raw::{c_char, c_int};
-use std::{env, mem, process, ptr};
+use std::process;
 
-use scallop::bash::bash_main;
+use scallop::bash::shell;
 
 fn main() {
-    let argv_strs: Vec<CString> = env::args().map(|s| CString::new(s).unwrap()).collect();
-    let mut argv_ptrs: Vec<*mut c_char> = argv_strs.iter().map(|s| s.as_ptr() as *mut _).collect();
-    argv_ptrs.push(ptr::null_mut());
-    let argv = argv_ptrs.as_ptr() as *mut _;
-    let argc: c_int = argv_strs.len().try_into().unwrap();
-    mem::forget(argv_strs);
-    mem::forget(argv_ptrs);
-
-    let env_strs: Vec<CString> = env::vars()
-        .map(|(key, val)| format!("{}={}", key, val))
-        .map(|s| CString::new(s).unwrap())
-        .collect();
-    let mut env_ptrs: Vec<*mut c_char> = env_strs.iter().map(|s| s.as_ptr() as *mut _).collect();
-    env_ptrs.push(ptr::null_mut());
-    let env = env_ptrs.as_ptr() as *mut _;
-    mem::forget(env_strs);
-    mem::forget(env_ptrs);
-
-    let ret = unsafe { bash_main(argc, argv, env) };
-    process::exit(ret)
+    process::exit(shell())
 }
