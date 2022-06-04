b4c193aa7f2f8183a7d8d757c95f7ac039a6a525
Tim Harder <radhermit@gmail.com>
2022-06-04 04:14:16 -0600

    bash: provide access to enabled shell options used with `set`
    In addition to those used with `shopt`.

--- src/bash.rs
+++ src/bash.rs
@@ -7,8 +7,14 @@ mod internal;
 // export bash API for external usage
 pub use internal::*;
 
-/// Return the set of currently enabled shell options.
-pub fn shell_opts() -> HashSet<String> {
+/// Return the set of enabled shell options used with the `set` builtin.
+pub fn set_opts() -> HashSet<String> {
+    let opts = string_value("SHELLOPTS").unwrap();
+    opts.split(':').map(|s| s.to_string()).collect()
+}
+
+/// Return the set of enabled shell options used with `shopt` builtin.
+pub fn shopt_opts() -> HashSet<String> {
     let opts = string_value("BASHOPTS").unwrap();
     opts.split(':').map(|s| s.to_string()).collect()
 }
