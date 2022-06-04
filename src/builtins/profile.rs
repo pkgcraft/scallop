97cf3b7cd17178a198aae1964f879be48c8e950d
Tim Harder <radhermit@gmail.com>
2022-06-04 04:18:10 -0600

    builtins::profile: drop threading usage since bash isn't thread-friendly
    And it's not really necessary for the simple case.

--- src/builtins/profile.rs
+++ src/builtins/profile.rs
@@ -1,9 +1,6 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
-use std::thread;
 use std::time::{Duration, Instant};
 
-use crate::builtins::{Builtin, ExecStatus};
+use crate::builtins::{Builtin, ExecStatus, ScopedOptions};
 use crate::command::Command;
 use crate::{Error, Result};
 
@@ -15,39 +12,23 @@ pub(crate) fn run(args: &[&str]) -> Result<ExecStatus> {
         return Err(Error::Builtin("requires 1 or more args, got 0".into()));
     }
 
+    let mut loops = 0;
+    let mut elapsed = Duration::new(0, 0);
     let orig_cmd = args.join(" ");
-    let loops = Arc::new(AtomicUsize::new(0));
-    let loops_t = loops.clone();
     eprintln!("profiling: {orig_cmd}");
 
-    thread::spawn(move || -> Result<()> {
-        // force success so the shell doesn't exit prematurely while profiling
-        let cmd_str = format!("{orig_cmd} || :");
-        let cmd = Command::new(cmd_str, None)?;
-        loop {
-            cmd.execute().ok();
-            loops_t.fetch_add(1, Ordering::SeqCst);
-        }
-    });
-
+    // force success so the shell doesn't exit prematurely while profiling
+    let cmd_str = format!("{orig_cmd} || :");
+    let cmd = Command::new(cmd_str, None)?;
     let start = Instant::now();
-    thread::sleep(Duration::from_secs(3));
-    let elapsed = start.elapsed();
-    let loops = loops
-        .load(Ordering::SeqCst)
-        .try_into()
-        .map_err(|_| Error::Base("failed converting loops".to_string()))?;
-
-    match loops {
-        0 => {
-            eprintln!("elapsed {elapsed:?}, loops: 0");
-        }
-        n => {
-            let per_loop = elapsed / loops;
-            eprintln!("elapsed {elapsed:?}, loops: {n}, per loop: {per_loop:?}");
-        }
+    while elapsed.as_secs() < 3 {
+        cmd.execute().ok();
+        loops += 1;
+        elapsed = start.elapsed();
     }
 
+    let per_loop = elapsed / loops;
+    eprintln!("elapsed {elapsed:?}, loops: {loops}, per loop: {per_loop:?}");
     Ok(ExecStatus::Success)
 }
 
